65c9ab8fa4048f747e3c85c88444f8cf5e157066
ABID HUSSAIN SIDDHPURA <abidhussainsiddhpura2703@gmail.com>
2024-10-18 11:02:40 +0530

    deploy purpose

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r0)xj##*20y(uh%fsw(%73@(!u*u2w05d!^^ggdd%$(@8j-4)u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["devtest-wgde.onrender.com","127.0.0.1","localhost"]
 
 
 # Application definition
